cb1ce777b466abf550b818a75639912cdb6c8e4a
Ricardo Balbi <rbalbi@gmail.com>
2020-03-16 16:06:06 -0300

    feat(frontend): save dev

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
@@ -6,30 +7,92 @@ import './Sidebar.css';
 import './Main.css';
 
 function App() {
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      err => {
+        console.log(err);
+      },
+      {
+        timeout: 30000
+      }
+    );
+  }, []);
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    });
+
+    // RETIRAR DEPOIS DO TESTE
+    console.log(response);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
+        <form onSubmit={handleAddDev}>
           <div className="input-block">
             <label htmlFor="github_username">Usuário do GitHub</label>
-            <input name="github_username" id="github_username" required />
+            <input
+              name="github_username"
+              id="github_username"
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+            />
           </div>
 
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" />
+            <input
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
           </div>
 
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" />
+              <input
+                type="number"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" />
+              <input
+                type="number"
+                name="longitude"
+                id="longitude"
+                required
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              />
             </div>
           </div>
 
@@ -42,7 +105,7 @@ function App() {
           <li className="dev-item">
             <header>
               <img
-                src="https://www.outsystems.com/forgeCore/rest/ComponentThumbnail/GetURL_ComponentThumbnail?ProjectImageId=18741"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVEkOHzDuIqZYFw8gDyqgXITxVFJi8zbe3RFL43J-KkTxFbNwRGQ&s"
                 alt="Avatar do usuário"
               />
               <div className="user-info">
@@ -57,7 +120,7 @@ function App() {
           <li className="dev-item">
             <header>
               <img
-                src="https://www.outsystems.com/forgeCore/rest/ComponentThumbnail/GetURL_ComponentThumbnail?ProjectImageId=18741"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVEkOHzDuIqZYFw8gDyqgXITxVFJi8zbe3RFL43J-KkTxFbNwRGQ&s"
                 alt="Avatar do usuário"
               />
               <div className="user-info">
@@ -71,7 +134,7 @@ function App() {
           <li className="dev-item">
             <header>
               <img
-                src="https://www.outsystems.com/forgeCore/rest/ComponentThumbnail/GetURL_ComponentThumbnail?ProjectImageId=18741"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVEkOHzDuIqZYFw8gDyqgXITxVFJi8zbe3RFL43J-KkTxFbNwRGQ&s"
                 alt="Avatar do usuário"
               />
               <div className="user-info">
@@ -85,7 +148,7 @@ function App() {
           <li className="dev-item">
             <header>
               <img
-                src="https://www.outsystems.com/forgeCore/rest/ComponentThumbnail/GetURL_ComponentThumbnail?ProjectImageId=18741"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVEkOHzDuIqZYFw8gDyqgXITxVFJi8zbe3RFL43J-KkTxFbNwRGQ&s"
                 alt="Avatar do usuário"
               />
               <div className="user-info">
