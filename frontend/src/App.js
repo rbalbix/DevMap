df9d1b87c8edd9ea66ffd4adbf9e6a3f1b1a822f
Ricardo Balbi <rbalbi@gmail.com>
2020-03-16 22:23:28 -0300

    feat(frontend): finish dev map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,164 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import api from './services/api';
 
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
 function App() {
-  const [github_username, setGithubUsername] = useState('');
-  const [techs, setTechs] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+  const [devs, setDevs] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        const { latitude, longitude } = position.coords;
+    async function loadDevs() {
+      const response = await api.get('/devs');
 
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      err => {
-        console.log(err);
-      },
-      {
-        timeout: 30000
-      }
-    );
+      setDevs(response.data);
+    }
+
+    loadDevs();
   }, []);
 
-  async function handleAddDev(e) {
-    e.preventDefault();
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
 
-    const response = await api.post('/devs', {
-      github_username,
-      techs,
-      latitude,
-      longitude
-    });
-
-    // RETIRAR DEPOIS DO TESTE
-    console.log(response);
+    setDevs([...devs, response.data]);
   }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do GitHub</label>
-            <input
-              name="github_username"
-              id="github_username"
-              required
-              value={github_username}
-              onChange={e => setGithubUsername(e.target.value)}
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input
-              name="techs"
-              id="techs"
-              required
-              value={techs}
-              onChange={e => setTechs(e.target.value)}
-            />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                type="number"
-                name="latitude"
-                id="latitude"
-                required
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                type="number"
-                name="longitude"
-                id="longitude"
-                required
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVEkOHzDuIqZYFw8gDyqgXITxVFJi8zbe3RFL43J-KkTxFbNwRGQ&s"
-                alt="Avatar do usuário"
-              />
-              <div className="user-info">
-                <strong>Ricardo</strong>
-                <span>React, Node</span>
-              </div>
-            </header>
-            <p>Bio do GH</p>
-            <a href="https://bitbucket.com">Acessar perfil no GitHub</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVEkOHzDuIqZYFw8gDyqgXITxVFJi8zbe3RFL43J-KkTxFbNwRGQ&s"
-                alt="Avatar do usuário"
-              />
-              <div className="user-info">
-                <strong>Ricardo</strong>
-                <span>React, Node</span>
-              </div>
-            </header>
-            <p>Bio do GH</p>
-            <a href="https://bitbucket.com">Acessar perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVEkOHzDuIqZYFw8gDyqgXITxVFJi8zbe3RFL43J-KkTxFbNwRGQ&s"
-                alt="Avatar do usuário"
-              />
-              <div className="user-info">
-                <strong>Ricardo</strong>
-                <span>React, Node</span>
-              </div>
-            </header>
-            <p>Bio do GH</p>
-            <a href="https://bitbucket.com">Acessar perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVEkOHzDuIqZYFw8gDyqgXITxVFJi8zbe3RFL43J-KkTxFbNwRGQ&s"
-                alt="Avatar do usuário"
-              />
-              <div className="user-info">
-                <strong>Ricardo</strong>
-                <span>React, Node</span>
-              </div>
-            </header>
-            <p>Bio do GH</p>
-            <a href="https://bitbucket.com">Acessar perfil no GitHub</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
