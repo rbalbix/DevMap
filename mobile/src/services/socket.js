997c59d6ed3f8ba9968f5235ef41a6a2b8ccc1b6
Ricardo Balbi <rbalbi@gmail.com>
2020-03-21 19:17:53 -0300

    feat(socket): finish application

--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -1,8 +1,12 @@
 import socketio from 'socket.io-client';
 
-const socket = socketio('http://192.168.0.14:3333', {
+const socket = socketio('http://192.168.1.108:3333', {
   autoConnect: false
 });
+
+function subscribeToNewDevs(subscribeFunction) {
+  socket.on('new-dev', subscribeFunction);
+}
 
 function connect(latitude, longitude, techs) {
   socket.io.opts.query = {
@@ -19,4 +23,4 @@ function disconnect() {
   }
 }
 
-export { connect, disconnect };
+export { connect, disconnect, subscribeToNewDevs };
