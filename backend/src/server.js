625997695c2d65a4d413560e0e19f0a9e78fabee
Ricardo Balbi <rbalbi@gmail.com>
2020-03-21 11:55:38 -0300

    feat(socket): add socket config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,15 @@
+const http = require('http');
 const app = require('./app');
+const { setupWebSocket } = require('./websocket');
+
+// To linten websocket protocol
+const server = http.Server(app);
+setupWebSocket(server);
 
 require('../src/database').connect();
 
 // ease to deploy || Verify the environment
 const PORT = process.env.PORT || 3333;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log('@port', PORT);
 });
